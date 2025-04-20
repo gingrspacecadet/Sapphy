ffa55a28e2af65d88018ad1ee6c48b946594434a
gingrspacecadet <gingrspacecadet@github.com>
2025-04-20 17:20:25 +0100

    update

--- functions/worker.js
+++ functions/worker.js
@@ -1,3 +1,5 @@
+import { getUserFromCookies, hashPassword, verifyPassword } from "./utils.js";
+
 export async function onRequest(context) {
   const { request, env } = context;
   const { method } = request;
@@ -212,37 +214,5 @@ export async function onRequest(context) {
         headers: { "Content-Type": "application/json" },
       });
     }
-  }  
-
-  // Password hashing
-  async function hashPassword(pw) {
-    const enc = new TextEncoder().encode(pw);
-    const hash = await crypto.subtle.digest("SHA-256", enc);
-    return Array.from(new Uint8Array(hash))
-      .map((b) => b.toString(16).padStart(2, "0"))
-      .join("");
-  }
-
-  // Password verification
-  async function verifyPassword(input, stored) {
-    return (await hashPassword(input)) === stored;
-  }
-
-  // Get user from email/password cookies
-  async function getUserFromCookies(request, env) {
-    const cookieHeader = request.headers.get("Cookie") || "";
-    const cookies = Object.fromEntries(cookieHeader.split("; ").map(c => c.split("=")));
-
-    const email = decodeURIComponent(cookies.email || "");
-    const storedHash = cookies.password || "";
-
-    if (!email || !storedHash) return null;
-
-    const user = await env.DB.prepare("SELECT * FROM users WHERE email = ?")
-      .bind(email).first();
-
-    if (!user || user.password !== storedHash) return null;
-
-    return user;
   }
 }
